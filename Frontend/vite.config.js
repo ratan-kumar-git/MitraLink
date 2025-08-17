0b76e91b3f4e192f61b0f5691d460b43c7d8ca54
Ratan Kumar <krratan80@gmail.com>
2025-08-17 18:50:37 +0530

    add menifesto.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "MitraLink",
+        short_name: "MitraLink",
+        description: "A real-time chat app built with React and Express",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1a1a1a",
+        icons: [
+          {
+            src: "/mitrLink.webp",
+            sizes: "192x192",
+            type: "image/webp"
+          },
+          {
+            src: "/mitrLink.webp",
+            sizes: "512x512",
+            type: "image/webp"
+          }
+        ]
+      }
+    })
+  ],
 })
